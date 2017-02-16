ea8d89f0bec51ccb8d80a9b0f4bf70d7dfd29687
Nicolas Antor <nicolas.antor@outlook.com>
2017-02-16 20:57:37 +0900

    Finished basic implementation

--- Program.cs
+++ Program.cs
@@ -15,28 +15,60 @@ namespace ConsoleApplication
             string infile = @"25kwordsin.csv";
 
             string[] words = System.IO.File.ReadAllLines(infile);
-            Dictionary<string, string> translations = new Dictionary<string, string>();
+            Dictionary<string, List<string>> translations = new Dictionary<string, List<string>>();
 
+            int cnt = 1;
             foreach (string word in words) {
-                string translation = translate(word);
+                if (cnt > 30)
+                    break;
 
-                if (translation != null) 
+                List<string> translation = translate(word);
+
+                System.Console.WriteLine("[" + cnt + "] Get word '" + word + "'...");
+
+                if (translation != null) {
                     translations.Add(word, translation);
-                else
+                } else {
                     System.Console.WriteLine("WARNING: '" + word + "' not found...");
+                    translations.Add(word, new List<string>() {""});
+                }
+
+                cnt++;
             }
 
+            // write result to a file
+            System.IO.File.WriteAllLines("outfile.csv", translations
+                            .Select(x => x.Key + ";" + toNumberListString(x.Value).Replace("&nbsp","")));
+
 	    }
+
+        /// <SUMMARY>
+        /// Transforms a list of Strings in an enumberated string
+        /// eg. "1. hallo 2. test 3. jojo"
+        /// </summary>
+        /// <returns>Enumerated String</returns>
+        /// <param name="inList">List of strings to work with</param>
+        static string toNumberListString(List<string> inList) 
+        {
+            if (inList.Count == 1)
+                return inList.First();
+
+            string retString = "";
+            for (int i=1; i <= Math.Min(inList.Count, 5); i++)
+                retString += i + ". " + inList[i-1] + " ";
+
+            return retString;
+        }
 
         /// <SUMMARY>
         /// Tries to look up a japanese word on wadoku
         /// </summary>
         /// <returns>The words translation if found, null else</returns>
         /// <param name="word">The japanese word to search for</param>
-        static string translate(string word)
+        static List<string> translate(string word)
         {
             string url = "https://www.wadoku.de/search/" + word;
-            string translation = "";
+            List<string> translations = new List<string>();
 
             var html = new HtmlDocument();
             var httpClient = new HttpClient();
@@ -53,14 +85,16 @@ namespace ConsoleApplication
 
                 // for each specific translation, only get the first one
                 foreach(HtmlNode sense in section.Descendants("span")
-                                                .Where(x => x.GetAttributeValue("class", "").Equals("sense")))
-                    translation += sense.InnerHtml.Split(';').First();
-            } catch(Exception e) {
+                                                 .Where(x => x.GetAttributeValue("class", "").Equals("sense"))) 
+                {
+                    translations.Add(sense.InnerText.Split(';')[1].Replace(".",""));
+                }
+            } catch(Exception) {
                 // we couldn't find a translation
                 return null;
             }
 
-            return translation;
+            return translations;
         }
     }
 }
